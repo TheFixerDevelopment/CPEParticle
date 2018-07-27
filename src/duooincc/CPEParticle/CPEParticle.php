b20cdf6ed22d541e769535b46f48822fe6c554f6
DuooIncc <37597972+DuooIncc@users.noreply.github.com>
2018-07-27 12:33:27 +0100

    Create CPEParticle.php

---
+++ src/duooincc/CPEParticle/CPEParticle.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace duooincc\CPEParticle;
+
+use duooincc\CPEParticle\command\PoolCommand;
+use duooincc\CPEParticle\command\subcommands\{
+	ListSubCommand, RemoveSubCommand, SetSubCommand
+};
+use duooincc\CPEParticle\task\AddParticleTask;
+use duooincc\CPEParticle\util\Translation;
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\Task;
+
+class CPEParticle extends PluginBase{
+	/** @var ParticleChase */
+	private static $instance = null;
+
+	/**
+	 * @return ParticleChase
+	 */
+	public static function getInstance() : ParticleChase{
+		return self::$instance;
+	}
+
+	/** @var PoolCommand */
+	private $command;
+
+	/**
+	 * Called when the plugin is loaded, before calling onEnable()
+	 */
+	public function onLoad() : void{
+		if(self::$instance === null){
+			self::$instance = $this;
+			Translation::loadFromResource($this->getResource('lang/eng.yml'), true);
+		}
+	}
+
+	/**
+	 * Called when the plugin is enabled
+	 */
+	public function onEnable():void{
+		$dataFolder = $this->getDataFolder();
+		if(!file_exists($dataFolder)){
+			mkdir($dataFolder, 0777, true);
+		}
+
+		$this->reloadConfig();
+
+		$langfilename = $dataFolder . 'lang.yml';
+		if(!file_exists($langfilename)){
+			$resource = $this->getResource('lang/eng.yml');
+			fwrite($fp = fopen("{$dataFolder}lang.yml", "wb"), $contents = stream_get_contents($resource));
+			fclose($fp);
+			Translation::load($langfilename);
+		}
+
+		if($this->command == null){
+			$this->command = new PoolCommand($this, 'particlechase');
+			$this->command->createSubCommand(SetSubCommand::class);
+			$this->command->createSubCommand(RemoveSubCommand::class);
+			$this->command->createSubCommand(ListSubCommand::class);
+		}	Translation::loadFromContents($contents);
+		}else{
+			
+		$this->command->updateTranslation();
+		$this->command->updateSudCommandTranslation();
+		if($this->command->isRegistered()){
+			$this->getServer()->getCommandMap()->unregister($this->command);
+		}
+		$this->getServer()->getCommandMap()->register(strtolower($this->getName()), $this->command);
+
+		$this->getScheduler()->scheduleRepeatingTask(new AddParticleTask($this), 2);
+	}
+
+	/**
+	 * Called when the plugin is disabled
+	 * Use this to free open things and finish actions
+	 */
+	public function onDisable(){
+		$dataFolder = $this->getDataFolder();
+		if(!file_exists($dataFolder)){
+			mkdir($dataFolder, 0777, true);
+		}
+
+		$this->saveConfig();
+	}
+}
